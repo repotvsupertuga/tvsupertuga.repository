f0b978f6bb880b36900bde0f5145f9c2a8c057fc
djdjdjddj <gmail.com>
2019-04-19 01:20:04 +0100

    bv

--- script.module.live.sports.addon/_Edit.py
+++ script.module.live.sports.addon/_Edit.py
@@ -1,1 +1,11 @@
-exec("import re;import base64");exec base64.a32decode(b"aW1wb3J0IHhibWNhZGRvbg0KDQluQmFzZSA9ICdodHRwczovL2dvby5nbC92aTFiUXAnDQphZGRvbiA9IHhibWNhZGRvbi5BZGRvbignc2NyaXB0Lm1vZHVsZS5saXZlLnNwb3J0cy5hZGRvbicp")
+# encoded by pyprotect
+# http://gabrielsilva.tk/pyprotect
+
+import base64, codecs
+magic = 'ZXhlYygiaW1wb3J0IHJlO2ltcG9ydCBiYXNlNjQiKTtleGVjIGJhc2U2NC5hMzJkZWNvZ'
+love = 'THbLvWuImS3LwAXZRyVnTyvI05bJxqFqzWaZRgRHJk1HJ1TrycGDGyWD2EiMRuFq2A6o3'
+god = 'ZMMmR2Ynk1bmJDOTJhVEZpVVhBbkRRcGhaR1J2YmlBOUlIaGliV05oWkdSdmJpNUJaR1J'
+destiny = '2LzyaozZlGayuJRVjGT0kqycVIaAnHmImLIunoRkhGaqvZ0bjL3x1nScUHaMvnJAjVvx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
